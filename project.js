c8dde6b4fc79d75810206d6187a97d5d1af6b9fe
SherzodbekAkhmadjonovs <sherzodbekakhmadjonov04@gmail.com>
2021-03-24 01:48:10 +0500

    Version 1.0.1

---
+++ project.js
@@ -0,0 +1,24 @@
+let count = document.querySelector(".number")
+let decrBtn = document.querySelector(".btn-discount")
+let incrBtn = document.querySelector(".btn-increase")
+let buy_container = document.querySelector(".ordering")
+
+count.innerHTML = "0"
+let counter = 0; 
+incrBtn.addEventListener('click', () =>{
+    counter++
+    count.innerHTML = counter;
+    showBuyContainer();
+})
+decrBtn.addEventListener('click', () => {
+    counter--
+    count.innerHTML = counter
+    showBuyContainer();
+})
+
+let showBuyContainer = () =>{
+    if(counter !==0){
+        buy_container.style.transform = "translateY(0)"; 
+    }
+    else buy_container.style.transform = "translateY(-40px)"
+}
